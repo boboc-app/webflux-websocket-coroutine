8e6016253aaee4b1432b977acd0aa0cc1cdf2cff
Bo-kang <53891564+Bo-kang@users.noreply.github.com>
2024-11-26 20:23:23 +0900

    Release/1.2.0 (#13)
    * Feat/websocket extension (#6)
    
    * feat: add logic for default handler
    
    * feat: add send string message method
    
    * fix: modify architecture of DefaultWebSocketHandler
    
    * minor: remove unnecessary classes
    
    * minor: deploy information
    
    * minor: modify group name
    
    ---------
    
    Co-authored-by: isk-bokang <bo-kang@iskra.world>
    
    * Minor/example (#8)
    
    * minor: add buildSrc
    
    * minor: add example
    
    * minor: add example codes
    
    ---------
    
    Co-authored-by: isk-bokang <bo-kang@iskra.world>
    
    * minor: add dependency information for pom.xml
    
    * minor: modify version
    
    * minor: add dependency information
    
    * minor: add read me (#9)
    
    Co-authored-by: isk-bokang <bo-kang@iskra.world>
    
    * minor: upgrade version
    
    * minor: clean up implementation
    
    * Feat/version up to 21 (#12)
    
    * feat: version up to java 21
    
    * minor: remove examples
    
    Signed-off-by: isk-bokang <bo.kang@iskra.world>
    
    * minor: add demo project link
    
    Signed-off-by: isk-bokang <bo.kang@iskra.world>
    
    ---------
    
    Signed-off-by: isk-bokang <bo.kang@iskra.world>
    Co-authored-by: isk-bokang <bo.kang@iskra.world>
    
    ---------
    
    Signed-off-by: isk-bokang <bo.kang@iskra.world>
    Co-authored-by: isk-bokang <bo-kang@iskra.world>
    Co-authored-by: isk-bokang <bo.kang@iskra.world>

--- modules/webflux-websocket-coroutine/build.gradle.kts
+++ modules/webflux-websocket-coroutine/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     signing
     id("com.gradleup.nmcp").version("0.0.7")
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
     `maven-publish`
 }
 
@@ -17,7 +17,7 @@ val artifactName = "webflux-websocket-coroutine"
 val groupName = app.boboc.Deps.groupName
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -35,7 +35,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
@@ -61,7 +61,6 @@ val sourcesJar = tasks.register("sourcesJar", Jar::class) {
 }
 
 val deployJar = tasks.register("deployJar", Jar::class) {
-
     archiveClassifier = "sources"
     sourceSets["main"].allSource
     group = "deploy"
