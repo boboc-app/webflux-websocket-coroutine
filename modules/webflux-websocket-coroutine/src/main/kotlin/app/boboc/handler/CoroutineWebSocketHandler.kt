f6fa244053cc7ad3b550665b5fffbfb379f41ab1
Bo-kang <53891564+Bo-kang@users.noreply.github.com>
2025-04-14 16:27:24 +0900

    feat: add open close functions (#14)
    Signed-off-by: isk-bokang <bo.kang@iskra.world>
    Co-authored-by: isk-bokang <bo.kang@iskra.world>

--- modules/webflux-websocket-coroutine/src/main/kotlin/app/boboc/handler/CoroutineWebSocketHandler.kt
+++ modules/webflux-websocket-coroutine/src/main/kotlin/app/boboc/handler/CoroutineWebSocketHandler.kt
@@ -1,5 +1,6 @@
 package app.boboc.handler
 
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactor.mono
 import org.springframework.web.reactive.socket.WebSocketHandler
@@ -10,9 +11,13 @@ import reactor.core.publisher.Mono
 abstract class CoroutineWebSocketHandler : WebSocketHandler {
     private fun receiveMessage(session: WebSocketSession): Mono<Void> {
         return mono {
+            onOpen(session)
             session.receive().map {
                 it.retain()
             }.asFlow()
+                .onCompletion {
+                    onClose(session)
+                }
                 .collect {
                     coroutineHandle(session, it)
                 }
@@ -20,6 +25,10 @@ abstract class CoroutineWebSocketHandler : WebSocketHandler {
     }
 
     abstract suspend fun coroutineHandle(session: WebSocketSession, message: WebSocketMessage)
+
+    open suspend fun onOpen(session: WebSocketSession) = Unit
+
+    open suspend fun onClose(session: WebSocketSession) = Unit
 
     override fun handle(session: WebSocketSession): Mono<Void> {
         return receiveMessage(session)
